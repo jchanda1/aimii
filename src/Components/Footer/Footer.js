9b313d29a67bf505787e10c825c35818037d6cda
jchanda1 <74137050+jchanda1@users.noreply.github.com>
2024-06-01 19:14:05 +0100

    formatting

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,31 +6,9 @@ import { newEmployeeActions } from "../../store/newEmployeeSlice";
 const Footer = () => {
   const dispatch = useDispatch();
 
-  //   const useStyles = makeStyles((theme) => ({
-  //     footer: {
-  //       position: "fixed",
-  //       bottom: 0,
-  //       width: "100%",
-  //       height: 60,
-  //       textAlign: "center",
-  //     },
-  //   }));
-
   const onClickHandler = (e) => {
     dispatch(newEmployeeActions.toggleIsVisible());
   };
-
-  //   const classes = useStyles();
-
-  //   return (
-  //     <Grid container spacing={1} className={classes.footer}>
-  //       <Grid item xs={5} />
-  //       <Grid item xs={2}>
-  //         <MyButton onClickHandler={onClickHandler}>New User+</MyButton>
-  //       </Grid>
-  //       <Grid item xs={5} />
-  //     </Grid>
-  //   );
 
   return (
     <AppBar
